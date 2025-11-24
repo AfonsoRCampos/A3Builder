e035a3dac9af860968cc038ac38a3d217239f6c1
Afonso Ribeiro Campos <acampos@lusospace.com>
2025-11-24 12:02:00 +0000

    Delete .next directory

--- .next/server/pages/api/a3s/[series]/versions/[version].js
+++
@@ -1,5 +0,0 @@
-var R=require("../../../../../chunks/[turbopack]_runtime.js")("server/pages/api/a3s/[series]/versions/[version].js")
-R.c("server/chunks/[root-of-the-server]__3182970e._.js")
-R.c("server/chunks/[root-of-the-server]__93e8e522._.js")
-R.m(32455)
-module.exports=R.m(32455).exports
--- .next/static/chunks/26dbf410d6182d10.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/d561eeef549134ef.js",
-  "static/chunks/e868cd8b76190d08.js",
-  "static/chunks/turbopack-39585e566b9d29b4.js"
-])
